4286633b1daacde828ce86fbe48cffc8cd49658f
milen06 <emamaleni55@gmail.com>
2025-10-23 01:15:19 +0800

    Implement code changes to enhance functionality and improve performance

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,8 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-        'https://wishlist-book-fe.vercel.app'
+        'https://wishlist-book-fe.vercel.app',
+        'https://libralist-app.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
